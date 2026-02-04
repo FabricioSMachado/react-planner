bdfff698caf28aae61942fcccd9fc327a20ea0ec
Fabricio Machado <fabricio.s.machados@gmail.com>
2026-02-04 03:09:10 -0300

    formando html

---
+++ aristopoles/src/App.js
@@ -0,0 +1,138 @@
+
+
+function App() {
+  return [
+
+    <div class="planner">
+  <header class="planner-header-datas">
+
+  </header>
+
+  <main class="planner-semana">
+    <section class="dia-semana">
+      <header class="dia-semana-header">
+        Segunda-feira
+      </header>
+
+      <ul class="lista-tarefas">
+        <li class="tarefa">
+          Tarefa 1
+        </li>
+        <li class="tarefa">
+          Tarefa 2
+        </li>
+      </ul>
+
+      <button class="add-tarefa">+</button>
+    </section>
+
+    <section class="dia-semana">
+      <header class="dia-semana-header">
+        Terça-feira
+      </header>
+
+      <ul class="lista-tarefas">
+        <li class="tarefa">
+          Tarefa 1
+        </li>
+        <li class="tarefa">
+          Tarefa 2
+        </li>
+      </ul>
+
+      <button class="add-tarefa">+</button>
+    </section>
+
+    <section class="dia-semana">
+      <header class="dia-semana-header">
+        Quarta-feira
+      </header>
+
+      <ul class="lista-tarefas">
+        <li class="tarefa">
+          Tarefa 1
+        </li>
+        <li class="tarefa">
+          Tarefa 2
+        </li>
+      </ul>
+
+      <button class="add-tarefa">+</button>
+    </section>
+
+    <section class="dia-semana">
+      <header class="dia-semana-header">
+        Quinta-feira
+      </header>
+
+      <ul class="lista-tarefas">
+        <li class="tarefa">
+          Tarefa 1
+        </li>
+        <li class="tarefa">
+          Tarefa 2
+        </li>
+      </ul>
+
+      <button class="add-tarefa">+</button>
+    </section>
+
+    <section class="dia-semana">
+      <header class="dia-semana-header">
+        Sexta-feira
+      </header>
+
+      <ul class="lista-tarefas">
+        <li class="tarefa">
+          Tarefa 1
+        </li>
+        <li class="tarefa">
+          Tarefa 2
+        </li>
+      </ul>
+
+      <button class="add-tarefa">+</button>
+    </section>
+
+    <section class="dia-semana">
+      <header class="dia-semana-header">
+        Sábado
+      </header>
+
+      <ul class="lista-tarefas">
+        <li class="tarefa">
+          Tarefa 1
+        </li>
+        <li class="tarefa">
+          Tarefa 2
+        </li>
+      </ul>
+
+      <button class="add-tarefa">+</button>
+    </section>
+
+    <section class="dia-semana">
+      <header class="dia-semana-header">
+        Domingo
+      </header>
+
+      <ul class="lista-tarefas">
+        <li class="tarefa">
+          Tarefa 1
+        </li>
+        <li class="tarefa">
+          Tarefa 2
+        </li>
+      </ul>
+
+      <button class="add-tarefa">+</button>
+    </section>
+
+    
+  </main>
+</div>
+
+  ]
+}
+
+export default App;
